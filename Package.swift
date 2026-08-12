4bbec09be01532bcf511dd1e24e9b3f4f16a17d5
Juan Ortega <jfoa92@hotmail.com>
2026-08-12 14:09:37 -0600

    Fix CI: Downgrade KeyboardShortcuts to 1.17.0 for Swift 5.10 compatibility

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v14),
     ],
     dependencies: [
-        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "2.2.2")
+        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "1.16.1")
     ],
     targets: [
         .executableTarget(
